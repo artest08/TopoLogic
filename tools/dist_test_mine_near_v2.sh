0af1833d2052bd9d41cefb10c1267ece328a9742
esat <esatkalfaoglu@gmail.com>
2025-11-03 13:19:37 +0300

    dist test near v2 bug fix again

--- tools/dist_test_mine_near_v2.sh
+++ tools/dist_test_mine_near_v2.sh
@@ -16,4 +16,4 @@ mkdir -p ${WORK_DIR}/test
 ~/containers/python_topomlp -m torch.distributed.run --nproc_per_node=$GPUS --master_port=$PORT \
     tools/test.py $CONFIG $CHECKPOINT --launcher pytorch \
     --out-dir ${WORK_DIR}/test --eval openlane_v2 ${@:3} \
-    2>&1 | tee ${WORK_DIR}/test.${CHECKPOINT_NAME}.log
+    2>&1 | tee ${WORK_DIR}/test/test.${CHECKPOINT_NAME}.log
